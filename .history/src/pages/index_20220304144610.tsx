c2871c0f1161a033b2b98d8a2dbbe42b8953bd16
unknown <viniciusserlimpa@gmail.com>
2022-03-04 14:51:51 -0300

    estilizações globais

---
+++ .history/src/pages/index_20220304144610.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import './styles/global.scss'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | vl.news</title>
+      </Head>
+      <h1>OLAAAAAAAA</h1>
+    </>
+  )
+}
---
+++ .history/src/pages/index_20220304144637.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | vl.news</title>
+      </Head>
+      <h1>OLAAAAAAAA</h1>
+    </>
+  )
+}
