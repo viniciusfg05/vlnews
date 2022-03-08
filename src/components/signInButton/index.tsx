8ccbd29b6df9570f64a45d4966258e7199916645
vinicius <vinicius.fg05@gmail.com>
2022-03-07 22:31:44 -0300

    salvando

--- src/components/signInButton/index.tsx
+++ src/components/signInButton/index.tsx
@@ -20,4 +20,15 @@ export function SingInButton() {
       Sing in with GitHub
     </button>
   );
-} 
+}
+
+// #Stripe
+// STRIPE_API_KEY=sk_test_51KaMv1F0aa4KZVxjGUEk1dxjod6HYqvIxTrGXAAtPXoGoSfyRuc51u7QFKbfsMsEl4qLJpWYxvQstHp24QmgkfTF00AqI0OplI
+// NEXT_PUBLIC_STRIPE_PUBLIC_KEY=pk_test_51KaMv1F0aa4KZVxjEb9DVL9ZkjFOZ4TnuN3bnSIfG7yvCOAIxUFDIfMC7y6BijB1M8ddrGeixfIWrqv7W92BuKPE00guOHa2DG
+// STRIPE_SUCCESS_URL=http://localhost:3000/posts
+// STRIPE_CANCEL_URL=http://localhost:3000/
+
+// GITHUB_CLIENT_ID=6b7885690cc4c2f85503
+// GITHUB_CLIENT_SECRET=1f64bdcce9200bd6465eaf296fe19dc71533898d
+
+// FAUNA_DB_API_KEY=fnAEhBss9WACT7pyf9RLxKStC51-4C83WuIgxU7F
