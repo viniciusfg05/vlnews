ed3d45e131e0adb29905d1ede60f292c5bf33df0
unknown <viniciusserlimpa@gmail.com>
2022-03-17 12:28:28 -0300

    comentario

--- .history/src/pages/api/auth/[...nextauth]_20220317122209.ts
+++ .history/src/pages/api/auth/[...nextauth]_20220317122209.ts
@@ -45,7 +45,7 @@ export default NextAuth({
           )
         )
   
-            //ERRRRRRRRRRRRRRRRRRRRRRo no git
+  
         return {
           ...session,
           activeSubscription: userActiveSubscription
